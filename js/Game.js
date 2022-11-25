407949c4e70031f001f19cbc73bfc2fc07ee7cd6
m4jken <113009402+m4jken@users.noreply.github.com>
2022-11-25 15:50:04 +0100

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -24,12 +24,30 @@ class Game {
     eventHandler(div){
         div.addEventListener('click', event => {
         let eventTarget = event.target.getAttribute('data-value');
-        console.log(eventTarget);
-        div.classList.toggle('flip');
+
+        const numOfFlippedCardsNodeList = document.querySelectorAll('.flip:not(.matched-card)');
+        const numOfFlippedCards = Array.prototype.slice.call(numOfFlippedCardsNodeList)
+        
+       
+        if (!div.classList.contains('flip')) {
+            div.classList.add('flip');
+
+            const numOfFlippedCardsNodeList = document.querySelectorAll('.flip:not(.matched-card)');
+            const numOfFlippedCards = Array.prototype.slice.call(numOfFlippedCardsNodeList)
+
+            if (numOfFlippedCards.length == 2 && numOfFlippedCards[0].innerHTML == numOfFlippedCards[1].innerHTML) {
+                numOfFlippedCards[0].classList.add('matched-card');
+                numOfFlippedCards[1].classList.add('matched-card');
+            } else if (numOfFlippedCards.length == 2) {
+                setTimeout(() => {
+                    numOfFlippedCards[0].classList.remove('flip');
+                    numOfFlippedCards[1].classList.remove('flip');
+                }, 500)
+            }
+        }
         })
     }
     flipCard(){
         this.classList.toggle('flip');
-    
     }
 }
